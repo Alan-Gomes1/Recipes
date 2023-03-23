041925fb5cda4d10f4d13b302e7f91ca54e5c5ff
Alan-Gomes1 <alan.gomes.ag28@gmail.com>
2023-03-23 18:36:30 -0300

    Adicionando as models Category e Recipe

--- recipes/views.py
+++ recipes/views.py
@@ -12,4 +12,5 @@ def home(request):
 def recipe(request, id):
     return render(request, 'recipes/pages/recipe-view.html', context={
         'recipe': make_recipe(),
+        'is_detail_page': True,
     })
