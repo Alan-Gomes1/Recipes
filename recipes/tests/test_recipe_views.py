1bfecb5812c1f23d6ae115c20bc86e9ce0352058
Alan-Gomes1 <alan.gomes.ag28@gmail.com>
2023-04-07 17:59:23 -0300

    Testando se category e detail carregam a receita correta

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -7,44 +7,41 @@ from .test_recipe_base import RecipeTestBase
 
 class RecipeViewsTest(RecipeTestBase):
     def test_recipe_home_view_function_is_correct(self):
-        view = resolve(
-            reverse('recipes:home')
-        )
+        view = resolve(reverse('recipes:home'))
         self.assertIs(view.func, views.home)
 
     def test_recipe_home_view_returns_status_code_200_OK(self):
-        response = self.client.get(
-            reverse('recipes:home')
-        )
+        response = self.client.get(reverse('recipes:home'))
         self.assertEqual(response.status_code, 200)
 
     def test_recipe_home_view_loads_correct_template(self):
-        response = self.client.get(
-            reverse('recipes:home')
-        )
+        response = self.client.get(reverse('recipes:home'))
         self.assertTemplateUsed(response, 'recipes/pages/home.html')
 
     def test_recipe_home_template_shows_no_recipes_found_if_no_recipes(self):
-        response = self.client.get(
-            reverse('recipes:home')
-        )
+        response = self.client.get(reverse('recipes:home'))
         self.assertIn(
-            'No recipes found here',
-            response.content.decode('utf-8')
+            'No recipes found here', response.content.decode('utf-8')
         )
 
     def test_recipe_home_template_loads_recipes(self):
         # need a recipe this test
         self.make_recipe()
-        response = self.client.get(
-            reverse('recipes:home')
-        )
+        response = self.client.get(reverse('recipes:home'))
         content = response.content.decode('utf-8')
         response_context_recipes = response.context['recipes']
         self.assertIn('Recipe title', content)
         self.assertIn('10 Minutos', content)
         self.assertIn('5 Porções', content)
         self.assertEqual(len(response_context_recipes), 1)
+
+    def test_recipe_home_template_dont_load_recipes_not_published(self):
+        # need a recipe this test
+        self.make_recipe(is_publised=False)
+        response = self.client.get(reverse('recipes:home'))
+        self.assertIn(
+            'No recipes found here', response.content.decode('utf-8')
+        )
 
     def test_recipe_category_view_function_is_correct(self):
         view = resolve(
@@ -58,14 +55,46 @@ class RecipeViewsTest(RecipeTestBase):
         )
         self.assertEqual(response.status_code, 404)
 
-    def test_recipe_detail_view_function_is_correct(self):
-        view = resolve(
-            reverse('recipes:recipe', kwargs={'id': 2})
+    def test_recipe_category_template_loads_recipes(self):
+        # need a recipe this test
+        needed_title = 'this is a category test'
+        self.make_recipe(title=needed_title)
+        response = self.client.get(reverse('recipes:category', args=(1,)))
+        content = response.content.decode('utf-8')
+        self.assertIn(needed_title, content)
+
+    def test_recipe_category_template_dont_load_recipes_not_published(self):
+        # need a recipe this test
+        recipe = self.make_recipe(is_publised=False)
+        response = self.client.get(
+            reverse('recipes:recipe', kwargs={'id': recipe.category.id})
         )
+        self.assertEqual(response.status_code, 404)
+
+    def test_recipe_detail_view_function_is_correct(self):
+        view = resolve(reverse('recipes:recipe', kwargs={'id': 2}))
         self.assertIs(view.func, views.recipe)
 
     def test_recipe_detail_view_returns_404_if_no_recipes_found(self):
         response = self.client.get(
             reverse('recipes:recipe', kwargs={'id': 1000})
+        )
+        self.assertEqual(response.status_code, 404)
+
+    def test_recipe_detail_template_loads_the_correct_recipe(self):
+        # need a recipe this test
+        needed_title = 'this is a detail paige - it load one recipe'
+        self.make_recipe(title=needed_title)
+        response = self.client.get(
+            reverse('recipes:recipe', kwargs={'id': 1})
+        )
+        content = response.content.decode('utf-8')
+        self.assertIn(needed_title, content)
+
+    def test_recipe_detail_template_dont_load_recipe_not_published(self):
+        # need a recipe this test
+        recipe = self.make_recipe(is_publised=False)
+        response = self.client.get(
+            reverse('recipes:recipe', kwargs={'id': recipe.id})
         )
         self.assertEqual(response.status_code, 404)
