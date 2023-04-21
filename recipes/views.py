db43073c5915d5677261f5dc8c533dafa760b706
Alan-Gomes1 <alan.gomes.ag28@gmail.com>
2023-04-20 23:34:24 -0300

    Levantando erro 404 se o termo de busca não for enviado

--- recipes/views.py
+++ recipes/views.py
@@ -35,4 +35,9 @@ def recipe(request, id):
 
 
 def search(request):
+    search_term = request.GET.get('q')
+
+    if not search_term:
+        raise Http404()
+
     return render(request, 'recipes/pages/search.html')
