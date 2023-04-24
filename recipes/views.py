68601fdec08e68d49879b04bd1b1f5ce8b6fd38e
Alan-Gomes1 <alan.gomes.ag28@gmail.com>
2023-04-24 19:24:14 -0300

    Adicionando paginação nos templates

--- recipes/views.py
+++ recipes/views.py
@@ -2,13 +2,18 @@ from django.db.models import Q
 from django.http import Http404
 from django.shortcuts import get_object_or_404, render
 
+from utils.pagination import make_pagination
 from .models import Recipe
 
 
 def home(request):
     recipes = Recipe.objects.filter(is_published=True).order_by('-id')
+
+    page_obj, pagination_range = make_pagination(request, recipes, 9)
+
     return render(request, 'recipes/pages/home.html', context={
-        'recipes': recipes,
+        'recipes': page_obj,
+        'pagination_range': pagination_range
     })
 
 
@@ -21,8 +26,11 @@ def category(request, category_id):
     if not recipes:
         raise Http404('Not Found')
 
+    page_obj, pagination_range = make_pagination(request, recipes, 9)
+
     return render(request, 'recipes/pages/category.html', context={
-        'recipes': recipes,
+        'recipes': page_obj,
+        'pagination_range': pagination_range,
         'title': f'{recipes.first().category.name} - Category | '
     })
 
@@ -49,8 +57,12 @@ def search(request):
         is_published=True
     ).order_by('-id')
 
+    page_obj, pagination_range = make_pagination(request, recipes, 9)
+
     return render(request, 'recipes/pages/search.html', {
         'page_title': f'Search for "{search_term}" | ',
         'search_term': search_term,
-        'recipes': recipes,
+        'recipes': page_obj,
+        'pagination_range': pagination_range,
+        'additional_url_query': f'&q={search_term}',
     })
---
+++ utils/test_pagination.py
@@ -0,0 +1,58 @@
+from unittest import TestCase
+
+from utils.pagination import make_pagination_range
+
+
+class PaginationTest(TestCase):
+    def test_make_pagination_range_returns_a_pagination_range(self):
+        pagination = make_pagination_range(
+            page_range=list(range(1, 21)),
+            qty_pages=4,
+            current_page=1,
+        )['pagination']
+        self.assertEqual([1, 2, 3, 4], pagination)
+
+    def test_first_range_is_static_if_currency_page_is_less_than_middle_page(self):  # noqa: E501
+        pagination = make_pagination_range(
+            page_range=list(range(1, 21)),
+            qty_pages=4,
+            current_page=1,
+        )['pagination']
+        self.assertEqual([1, 2, 3, 4], pagination)
+
+        pagination = make_pagination_range(
+            page_range=list(range(1, 21)),
+            qty_pages=4,
+            current_page=3,
+        )['pagination']
+        self.assertEqual([2, 3, 4, 5], pagination)
+
+    def test_make_sure_middle_ranges_are_correct(self):
+        pagination = make_pagination_range(
+            page_range=list(range(1, 21)),
+            qty_pages=4,
+            current_page=10,
+        )['pagination']
+        self.assertEqual([9, 10, 11, 12], pagination)
+
+        pagination = make_pagination_range(
+            page_range=list(range(1, 21)),
+            qty_pages=4,
+            current_page=17,
+        )['pagination']
+        self.assertEqual([16, 17, 18, 19], pagination)
+
+    def test_make_pagination_range_is_static_when_last_page_is_next(self):
+        pagination = make_pagination_range(
+            page_range=list(range(1, 21)),
+            qty_pages=4,
+            current_page=19,
+        )['pagination']
+        self.assertEqual([17, 18, 19, 20], pagination)
+
+        pagination = make_pagination_range(
+            page_range=list(range(1, 21)),
+            qty_pages=4,
+            current_page=20,
+        )['pagination']
+        self.assertEqual([17, 18, 19, 20], pagination)
