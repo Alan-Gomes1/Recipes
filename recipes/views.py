85c0e538b8a313953fc068c8466bb36b0b3f6f91
Alan-Gomes1 <alan.gomes.ag28@gmail.com>
2023-04-25 22:09:58 -0300

    Adicionando arquivos estaticos e variaveis de ambiente com .env

--- recipes/views.py
+++ recipes/views.py
@@ -1,15 +1,20 @@
+import os
+
 from django.db.models import Q
 from django.http import Http404
 from django.shortcuts import get_object_or_404, render
 
 from utils.pagination import make_pagination
+
 from .models import Recipe
+
+PER_PAGE = os.environ.get('PER_PAGE', 6)
 
 
 def home(request):
     recipes = Recipe.objects.filter(is_published=True).order_by('-id')
 
-    page_obj, pagination_range = make_pagination(request, recipes, 9)
+    page_obj, pagination_range = make_pagination(request, recipes, PER_PAGE)
 
     return render(request, 'recipes/pages/home.html', context={
         'recipes': page_obj,
@@ -26,7 +31,7 @@ def category(request, category_id):
     if not recipes:
         raise Http404('Not Found')
 
-    page_obj, pagination_range = make_pagination(request, recipes, 9)
+    page_obj, pagination_range = make_pagination(request, recipes, PER_PAGE)
 
     return render(request, 'recipes/pages/category.html', context={
         'recipes': page_obj,
@@ -57,7 +62,7 @@ def search(request):
         is_published=True
     ).order_by('-id')
 
-    page_obj, pagination_range = make_pagination(request, recipes, 9)
+    page_obj, pagination_range = make_pagination(request, recipes, PER_PAGE)
 
     return render(request, 'recipes/pages/search.html', {
         'page_title': f'Search for "{search_term}" | ',
