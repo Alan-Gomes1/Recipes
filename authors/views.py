3f1cf90528fbbd2215570ed7a6d5aa0a45b9b923
Alan-Gomes1 <alan.gomes.ag28@gmail.com>
2023-05-01 11:52:21 -0300

    Criando um novo app chamado authors para usuários se registrarem

---
+++ authors/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from .forms import RegisterForm
+
+
+def register_view(request):
+    form = RegisterForm()
+    return render(request, 'authors/pages/register_view.html', {
+        'form': form,
+    })
