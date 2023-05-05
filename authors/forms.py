06133a2454c766b56e9a2665cc110493c3694f3b
Alan-Gomes1 <alan.gomes.ag28@gmail.com>
2023-05-05 11:41:43 -0300

    Validando os campos do formulario

--- authors/forms.py
+++ authors/forms.py
@@ -1,9 +1,23 @@
+import re
+
 from django import forms
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 
 
 def add_attr(field, attr_name, attr_new_val):
     field.widget.attrs[attr_name] = attr_new_val.strip()
+
+
+def strong_password(password):
+    regex = re.compile(r'^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{8,}$')
+
+    if not regex.match(password):
+        raise ValidationError(("""
+            Password must have at least one uppercase letter,
+            one lowercase letter and one number. The length should be
+            at last 8 characters."""),
+                              code='invalid')
 
 
 class RegisterForm(forms.ModelForm):
@@ -13,10 +27,12 @@ class RegisterForm(forms.ModelForm):
         add_attr(self.fields['email'], 'placeholder', 'Your e-mail')
         add_attr(self.fields['first_name'], 'placeholder', 'Ex.: John')
         add_attr(self.fields['last_name'], 'placeholder', 'Ex.: Doe')
-        add_attr(self.fields['password'], 'placeholder', 'Type your password here')
 
     password = forms.CharField(
         required=True,
+        widget=forms.PasswordInput(attrs={
+            'placeholder': 'Type your password here'
+        }),
         error_messages={
             'required': 'Password must not be empty'
         },
@@ -24,7 +40,8 @@ class RegisterForm(forms.ModelForm):
         Password must have at least one uppercase letter,
         one lowercase letter and one number. The length should be
         at last 8 characters.
-        """)
+        """),
+        validators=[strong_password]
     )
 
     confirm_password = forms.CharField(
@@ -63,3 +80,14 @@ class RegisterForm(forms.ModelForm):
             'email': 'E-mail',
             'password': 'Password',
         }
+
+    def clean(self):
+        cleaned_data = super().clean()
+
+        password = cleaned_data.get('password')
+        confirm_password = cleaned_data.get('confirm_password')
+
+        if password != confirm_password:
+            raise ValidationError({
+                'confirm_password': 'Password and confirm password must be equal'
+            })
--- authors/views.py
+++ authors/views.py
@@ -1,5 +1,6 @@
+from django.contrib import messages
 from django.http import Http404
-from django.shortcuts import render, redirect
+from django.shortcuts import redirect, render
 
 from .forms import RegisterForm
 
@@ -19,5 +20,10 @@ def register_create(request):
     POST = request.POST
     request.session['register_form_data'] = POST
     form = RegisterForm(POST)
+
+    if form.is_valid():
+        form.save()
+        messages.success(request, 'Your user is created, please log in.')
+        del request.session['register_form_data']
 
     return redirect('authors:register')
