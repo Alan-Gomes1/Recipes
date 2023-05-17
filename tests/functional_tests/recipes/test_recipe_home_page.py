03e8936e979d66f365a0f743b14d2e593c5d06ea
Alan-Gomes1 <alan.gomes.ag28@gmail.com>
2023-05-17 12:21:52 -0300

    Testando o formulário de registro do autor

--- tests/functional_tests/recipes/test_recipe_home_page.py
+++ tests/functional_tests/recipes/test_recipe_home_page.py
@@ -1,15 +1,51 @@
+from unittest.mock import patch
+
 import pytest
 from selenium.webdriver.common.by import By
-from unittest.mock import patch
+from selenium.webdriver.common.keys import Keys
 
 from .base import RecipeBaseFunctionalTest
 
 
 @pytest.mark.functional_test
 class RecipeHomePageFunctionalTest(RecipeBaseFunctionalTest):
-    @patch('recipes.views.PER_PAGE', new=2)
     def test_recipe_home_page_without_recipes_not_found_message(self):
         self.browser.get(self.live_server_url)
         body = self.browser.find_element(By.TAG_NAME, 'body')
-        self.sleep(5)
         self.assertIn('No recipes found here 😢', body.text)
+
+    @patch('recipes.views.PER_PAGE', new=2)
+    def test_recipe_search_input_can_find_correct_recipes(self):
+        recipes = self.make_recipe_in_batch()
+        title_needed = 'This is what I need'
+        recipes[0].title = title_needed
+        recipes[0].save()
+
+        self.browser.get(self.live_server_url)
+        search_input = self.browser.find_element(
+            By.XPATH,
+            '//input[@placeholder="Search for a recipe"]'
+        )
+        search_input.send_keys(recipes[0].title)
+        search_input.send_keys(Keys.ENTER)
+
+        self.assertIn(
+            title_needed,
+            self.browser.find_element(By.CLASS_NAME, 'main-content-list').text
+        )
+
+    @patch('recipes.views.PER_PAGE', new=2)
+    def test_recipe_home_page_pagination(self):
+        self.make_recipe_in_batch()
+
+        self.browser.get(self.live_server_url)
+        page2 = self.browser.find_element(
+            By.XPATH,
+            '//a[@aria-label="Go to page 2"]'
+        )
+        page2.click()
+
+        self.assertEqual(
+            len(self.browser.find_elements(
+                By.CLASS_NAME, 'recipe')), 2
+        )
