733c4536dbe81d43300cde0c71fdb7d764ffec96
Alan-Gomes1 <alan.gomes.ag28@gmail.com>
2023-06-26 10:37:44 -0300

    Adicionando o django-cors-headers

--- project/settings.py
+++ project/settings.py
@@ -16,6 +16,8 @@ from pathlib import Path
 
 from django.contrib.messages import constants
 
+from utils.environment import get_env_variable, parse_comma_sep_str_to_list
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -41,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework_simplejwt',
     'rest_framework',
     'recipes',
@@ -53,6 +56,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -171,7 +175,6 @@ REST_FRAMEWORK = {
     ),
 }
 
-
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
@@ -180,3 +183,8 @@ SIMPLE_JWT = {
     "SIGNING_KEY": os.environ.get('SECRET_KEY_JWT', 'INSECURE'),
     "AUTH_HEADER_TYPES": ("Bearer",),
 }
+
+# CORS
+CORS_ALLOWED_ORIGINS = parse_comma_sep_str_to_list(
+    get_env_variable('CORS_ALLOWED_ORIGINS')
+)
